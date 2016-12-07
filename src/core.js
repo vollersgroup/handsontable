f7e7077d582441e265dbdd0d211483e7c1c39595
Armin Pfurtscheller <armin.pfurtscheller@cropster.org>
2016-12-07 09:13:22 +0100

    Fix maximum call stack errors

--- src/core.js
+++ src/core.js
@@ -790,34 +790,10 @@ export default function Core(rootElement, userSettings, rootInstanceSymbol = fal
                   value = isUndefined(result.value) ? value : result.value;
                 }
               }
-              if (value !== null && typeof value === 'object') {
-                // when 'value' is array and 'orgValue' is null, set 'orgValue' to
-                // an empty array so that the null value can be compared to 'value'
-                // as an empty value for the array context
-                if (Array.isArray(value) && orgValue === null) orgValue = [];
-
-                if (orgValue === null || typeof orgValue !== 'object') {
-                  pushData = false;
-
-                } else {
-                  const orgValueSchema = duckSchema(Array.isArray(orgValue) ? orgValue : (orgValue[0] || orgValue));
-                  const valueSchema = duckSchema(Array.isArray(value) ? value : (value[0] || value));
-
-                  /* eslint-disable max-depth */
-                  if (isObjectEqual(orgValueSchema, valueSchema)) {
-                    value = deepClone(value);
-                  } else {
-                    pushData = false;
-                  }
-                }
-
-              } else if (orgValue !== null && typeof orgValue === 'object') {
-                pushData = false;
+              if (Array.isArray(value)) {
+                value = value.slice(0);
               }
-              if (pushData) {
-                setData.push([current.row, current.col, value]);
-              }
-              pushData = true;
+              setData.push([current.row, current.col, value]);
               current.col += 1;
             }
             current.row += 1;
